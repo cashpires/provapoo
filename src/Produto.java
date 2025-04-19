51b5d4fbc80b34ea6d19b7a328ceecff4829f285
cashpires <167040814+cashpires@users.noreply.github.com>
2025-04-19 12:13:23 -0300

    Produto.java

--- src/Produto.java
+++ src/Produto.java
@@ -1,8 +1,8 @@
 public class Produto {
-    int codigo;
-    String nome;
-    double valor;
-    int quantidade;
+    private int codigo;
+    private String nome;
+    private double valor;
+    private int quantidade;
 
     public Produto(int codigo, String nome, double valor, int quantidade) {
         this.codigo = codigo;
@@ -10,4 +10,16 @@ public class Produto {
         this.valor = valor;
         this.quantidade = quantidade;
     }
+
+    public int getCodigo() { return codigo; }
+    public void setCodigo(int codigo) { this.codigo = codigo; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public double getValor() { return valor; }
+    public void setValor(double valor) { this.valor = valor; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
 }
